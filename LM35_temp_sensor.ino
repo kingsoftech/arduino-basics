c49faec3c06e43e96a55b9661c6e44ca7acfd8ad
Oyero Abdullahi Surajudeen <39423180+kingsoftech@users.noreply.github.com>
2021-07-09 20:37:47 +0100

    Add files via upload

---
+++ LM35_temp_sensor.ino
@@ -0,0 +1,22 @@
+float temp;
+int ledPin = 7;
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(ledPin, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  temp = analogRead(A0);
+  // put your main code here, to run repeatedly:
+  temp = temp*0.48828125;
+  Serial.print("TEMPERATURE: ");
+  Serial.print(temp);
+  Serial.print("*C");
+  Serial.println();
+
+  delay(1000);
+  
+
+}
